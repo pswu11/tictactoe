1ab060ed3fd8ac477dc4236eaae9d535dcf4b29a
Pei-Shan Wu <pswu@protonmail.com>
2021-03-28 12:44:32 +0200

    add game instruction

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 	newGame()
 }
 
 func newGame() {
 	newBoard()
+	fmt.Println("Please take your position by typing the XY coordinate  (e.g. 13)")
 	for i := 0; i < 9; i++ {
 		if i%2 == 0 {
 			playerRound(1, i)
